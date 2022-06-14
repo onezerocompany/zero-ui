4fe9482d9f42388430ff4beb58da17f9e1459484
Luca Silverentand <luca@onezero.company>
2022-06-14 21:51:18 +0200

    added react wrapper package

---
+++ packages/zero-ui-react/src/components/stencil-generated/index.ts
@@ -0,0 +1,26 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@onezerocompany/zero-ui-web';
+
+import { defineCustomElements } from '@onezerocompany/zero-ui-web/loader';
+
+defineCustomElements();
+export const ButtonsSection = /*@__PURE__*/createReactComponent<JSX.ButtonsSection, HTMLButtonsSectionElement>('buttons-section');
+export const ColorsSection = /*@__PURE__*/createReactComponent<JSX.ColorsSection, HTMLColorsSectionElement>('colors-section');
+export const ContainerSection = /*@__PURE__*/createReactComponent<JSX.ContainerSection, HTMLContainerSectionElement>('container-section');
+export const GradientsSection = /*@__PURE__*/createReactComponent<JSX.GradientsSection, HTMLGradientsSectionElement>('gradients-section');
+export const InputsSection = /*@__PURE__*/createReactComponent<JSX.InputsSection, HTMLInputsSectionElement>('inputs-section');
+export const TypographySection = /*@__PURE__*/createReactComponent<JSX.TypographySection, HTMLTypographySectionElement>('typography-section');
+export const ZeroBar = /*@__PURE__*/createReactComponent<JSX.ZeroBar, HTMLZeroBarElement>('zero-bar');
+export const ZeroButton = /*@__PURE__*/createReactComponent<JSX.ZeroButton, HTMLZeroButtonElement>('zero-button');
+export const ZeroContainer = /*@__PURE__*/createReactComponent<JSX.ZeroContainer, HTMLZeroContainerElement>('zero-container');
+export const ZeroDivider = /*@__PURE__*/createReactComponent<JSX.ZeroDivider, HTMLZeroDividerElement>('zero-divider');
+export const ZeroGrid = /*@__PURE__*/createReactComponent<JSX.ZeroGrid, HTMLZeroGridElement>('zero-grid');
+export const ZeroHeader = /*@__PURE__*/createReactComponent<JSX.ZeroHeader, HTMLZeroHeaderElement>('zero-header');
+export const ZeroInput = /*@__PURE__*/createReactComponent<JSX.ZeroInput, HTMLZeroInputElement>('zero-input');
+export const ZeroScaffold = /*@__PURE__*/createReactComponent<JSX.ZeroScaffold, HTMLZeroScaffoldElement>('zero-scaffold');
+export const ZeroSection = /*@__PURE__*/createReactComponent<JSX.ZeroSection, HTMLZeroSectionElement>('zero-section');
+export const ZeroText = /*@__PURE__*/createReactComponent<JSX.ZeroText, HTMLZeroTextElement>('zero-text');
