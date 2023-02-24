2c1f1af79cad51b6e2b48da66dc582345d853fe8
Luca Silverentand <luca@onezero.company>
2023-02-24 17:37:59 +0100

    :tada: feat/new(firebase-errors): add display of firebase errors
    Signed-off-by: Luca Silverentand <luca@onezero.company>

--- packages/zero_flutter/lib/l10n/zeroui_localizations_en.dart
+++ packages/zero_flutter/lib/l10n/zeroui_localizations_en.dart
@@ -5,9 +5,6 @@ class ZeroUIAppLocalizationsEn extends ZeroUIAppLocalizations {
   ZeroUIAppLocalizationsEn([String locale = 'en']) : super(locale);
 
   @override
-  String get generalErrorMessage => 'Something went wrong. Please try again later.';
-
-  @override
   String get confirmButton => 'Confirm';
 
   @override
@@ -159,4 +156,325 @@ class ZeroUIAppLocalizationsEn extends ZeroUIAppLocalizations {
 
   @override
   String get resendVerificationEmailError => 'Failed to send verification email. Please try again later.';
+
+  @override
+  String get authErrorAdminRestrictedOperation => 'You are not authorized to perform this action.';
+
+  @override
+  String get authErrorArgumentError => 'Something is wrong with the information you provided.';
+
+  @override
+  String get authErrorAppNotAuthorized => 'The app is not authorized to access your account.';
+
+  @override
+  String get authErrorAppNotInstalled => 'Please install the app to continue.';
+
+  @override
+  String get authErrorCaptchaCheckFailed => 'Please verify that you are not a robot.';
+
+  @override
+  String get authErrorCodeExpired => 'The code you entered has expired. Please try again.';
+
+  @override
+  String get authErrorCorsUnsupported => 'This feature is not supported in your current browser.';
+
+  @override
+  String get authErrorCredentialAlreadyInUse => 'The account is already in use by another user.';
+
+  @override
+  String get authErrorCustomTokenMismatch => 'There is a problem with your login credentials.';
+
+  @override
+  String get authErrorRequiresRecentLogin => 'You need to login again to complete this action.';
+
+  @override
+  String get authErrorDependentSdkInitializedBeforeAuth => 'Authentication failed. Please try again.';
+
+  @override
+  String get authErrorDynamicLinkNotActivated => 'The link you are trying to access is no longer active.';
+
+  @override
+  String get authErrorEmailChangeNeedsVerification => 'You need to verify your new email address to complete this action.';
+
+  @override
+  String get authErrorEmailAlreadyInUse => 'This email is already associated with an account.';
+
+  @override
+  String get authErrorEmulatorConfigFailed => 'There is a problem with the emulator configuration.';
+
+  @override
+  String get authErrorExpiredActionCode => 'The code you entered has expired. Please try again.';
+
+  @override
+  String get authErrorCancelledPopupRequest => 'The popup request was cancelled.';
+
+  @override
+  String get authErrorInternalError => 'Authentication failed due to an internal error. Please try again.';
+
+  @override
+  String get authErrorInvalidApiKey => 'The API key you provided is invalid.';
+
+  @override
+  String get authErrorInvalidAppCredential => 'The app credentials are invalid.';
+
+  @override
+  String get authErrorInvalidAppId => 'The app ID is invalid.';
+
+  @override
+  String get authErrorInvalidUserToken => 'The user token is invalid. Please try logging in again.';
+
+  @override
+  String get authErrorInvalidAuthEvent => 'Authentication failed due to an invalid event.';
+
+  @override
+  String get authErrorInvalidCertHash => 'The certificate hash is invalid. Please try again.';
+
+  @override
+  String get authErrorInvalidVerificationCode => 'The verification code is invalid. Please try again.';
+
+  @override
+  String get authErrorInvalidContinueUri => 'The continue URL is invalid. Please try again.';
+
+  @override
+  String get authErrorInvalidCordovaConfiguration => 'The Cordova configuration is invalid. Please try again.';
+
+  @override
+  String get authErrorInvalidCustomToken => 'The custom token is invalid. Please try again.';
+
+  @override
+  String get authErrorInvalidDynamicLinkDomain => 'The dynamic link domain is invalid. Please try again.';
+
+  @override
+  String get authErrorInvalidEmail => 'The email address you entered is invalid. Please try again.';
+
+  @override
+  String get authErrorInvalidEmulatorScheme => 'The emulator scheme is invalid. Please try again.';
+
+  @override
+  String get authErrorInvalidCredential => 'The credential you provided is invalid. Please try again.';
+
+  @override
+  String get authErrorInvalidMessagePayload => 'The message payload is invalid. Please try again.';
+
+  @override
+  String get authErrorInvalidMultiFactorSession => 'The multi-factor session is invalid. Please try again.';
+
+  @override
+  String get authErrorInvalidOAuthClientId => 'The OAuth client ID is invalid. Please try again.';
+
+  @override
+  String get authErrorInvalidOAuthProvider => 'The OAuth provider is invalid. Please try again.';
+
+  @override
+  String get authErrorInvalidActionCode => 'The action code is invalid. Please try again.';
+
+  @override
+  String get authErrorUnauthorizedDomain => 'You are not authorized to access this domain.';
+
+  @override
+  String get authErrorWrongPassword => 'The password you entered is incorrect. Please try again.';
+
+  @override
+  String get authErrorInvalidPersistenceType => 'The persistence type is invalid. Please try again.';
+
+  @override
+  String get authErrorInvalidPhoneNumber => 'The phone number you entered is invalid. Please try again with a valid phone number.';
+
+  @override
+  String get authErrorInvalidProviderId => 'The provider ID is invalid. Please try again.';
+
+  @override
+  String get authErrorInvalidRecipientEmail => 'The recipient email address is invalid. Please try again.';
+
+  @override
+  String get authErrorInvalidSender => 'The sender is invalid. Please try again.';
+
+  @override
+  String get authErrorInvalidVerificationId => 'The verification ID is invalid. Please try again.';
+
+  @override
+  String get authErrorInvalidTenantId => 'The tenant ID is invalid. Please try again.';
+
+  @override
+  String get authErrorMultiFactorInfoNotFound => 'The multi-factor information was not found. Please try again.';
+
+  @override
+  String get authErrorMultiFactorAuthRequired => 'Multi-factor authentication is required to complete this.';
+
+  @override
+  String get authErrorMissingAndroidPkgName => 'The Android package name is missing. Please try again.';
+
+  @override
+  String get authErrorMissingAppCredential => 'The app credentials are missing. Please try again.';
+
+  @override
+  String get authErrorAuthDomainConfigRequired => 'The authentication domain configuration is required. Please try again.';
+
+  @override
+  String get authErrorMissingVerificationCode => 'The verification code is missing. Please try again.';
+
+  @override
+  String get authErrorMissingContinueUri => 'The continue URL is missing. Please try again.';
+
+  @override
+  String get authErrorMissingMultiFactorInfo => 'The multi-factor information is missing. Please try again.';
+
+  @override
+  String get authErrorMissingMultiFactorSession => 'The multi-factor session is missing. Please try again.';
+
+  @override
+  String get authErrorMissingPhoneNumber => 'The phone number is missing. Please try again.';
+
+  @override
+  String get authErrorMissingVerificationId => 'The verification ID is missing. Please try again.';
+
+  @override
+  String get authErrorAppDeleted => 'The app has been deleted. Please contact support for assistance.';
+
+  @override
+  String get authErrorAccountExistsWithDifferentCredential => 'An account already exists with this email address. Please try logging in with a different method.';
+
+  @override
+  String get authErrorNetworkRequestFailed => 'The network request failed. Please check your internet connection and try again.';
+
+  @override
+  String get authErrorNullUser => 'The user information is missing. Please try logging in again.';
+
+  @override
+  String get authErrorNoAuthEvent => 'Authentication failed due to missing information. Please try again.';
+
+  @override
+  String get authErrorNoSuchProvider => 'The provider does not exist. Please try again.';
+
+  @override
+  String get authErrorOperationNotAllowed => 'You are not allowed to perform this operation.';
+
+  @override
+  String get authErrorOperationNotSupportedInThisEnvironment => 'This operation is not supported in your current environment.';
+
+  @override
+  String get authErrorPopupBlocked => 'The popup was blocked. Please allow popups and try again.';
+
+  @override
+  String get authErrorPopupClosedByUser => 'The popup was closed by the user.';
+
+  @override
+  String get authErrorProviderAlreadyLinked => 'The provider is already linked to another account. Please try again.';
+
+  @override
+  String get authErrorQuotaExceeded => 'The quota has been exceeded. Please try again later.';
+
+  @override
+  String get authErrorRedirectCancelledByUser => 'The redirect was cancelled by the user.';
+
+  @override
+  String get authErrorRedirectOperationPending => 'The redirect operation is still pending. Please try again.';
+
+  @override
+  String get authErrorRejectedCredential => 'The credential was rejected. Please try again.';
+
+  @override
+  String get authErrorSecondFactorAlreadyInUse => 'The second factor is already in use. Please try again.';
+
+  @override
+  String get authErrorMaximumSecondFactorCountExceeded => 'The maximum number of second factors has been reached. Please try again.';
+
+  @override
+  String get authErrorTenantIdMismatch => 'The tenant ID does not match. Please try again.';
+
+  @override
+  String get authErrorTimeout => 'The operation timed out. Please try again.';
+
+  @override
+  String get authErrorUserTokenExpired => 'The user token has expired. Please try logging in again.';
+
+  @override
+  String get authErrorTooManyRequests => 'Too many requests. Please try again later.';
+
+  @override
+  String get authErrorUnauthorizedContinueUri => 'The continue URL is not authorized. Please try again.';
+
+  @override
+  String get authErrorUnsupportedFirstFactor => 'The first factor is not supported. Please try again.';
+
+  @override
+  String get authErrorUnsupportedPersistenceType => 'The persistence type is not supported. Please try again.';
+
+  @override
+  String get authErrorUnsupportedTenantOperation => 'The tenant operation is not supported. Please try again.';
+
+  @override
+  String get authErrorUnverifiedEmail => 'The email address has not been verified. Please check your email and follow the verification link.';
+
+  @override
+  String get authErrorUserCancelled => 'The user cancelled the operation.';
+
+  @override
+  String get authErrorUserNotFound => 'The user account was not found. Please try again.';
+
+  @override
+  String get authErrorUserDisabled => 'The user account has been disabled. Please contact support for assistance.';
+
+  @override
+  String get authErrorUserMismatch => 'The user account does not match the information provided. Please try again.';
+
+  @override
+  String get authErrorUserSignedOut => 'The user account has been signed out. Please log in again to continue.';
+
+  @override
+  String get authErrorWeakPassword => 'The password is too weak. Please choose a stronger password.';
+
+  @override
+  String get authErrorWebStorageUnsupported => 'Web storage is not supported in your current browser.';
+
+  @override
+  String get authErrorAlreadyInitialized => 'Authentication has already been initialized.';
+
+  @override
+  String get httpsErrorCancelled => 'The action was cancelled or interrupted by the user.';
+
+  @override
+  String get httpsErrorInvalidArgument => 'The request was not valid. Please check your input and try again.';
+
+  @override
+  String get httpsErrorDeadlineExceeded => 'The operation timed out. Please try again later.';
+
+  @override
+  String get httpsErrorNotFound => 'The requested resource could not be found.';
+
+  @override
+  String get httpsErrorAlreadyExists => 'The resource already exists. Please choose a different name or ID.';
+
+  @override
+  String get httpsErrorPermissionDenied => 'You do not have permission to perform this action.';
+
+  @override
+  String get httpsErrorResourceExhausted => 'The resource has been exhausted. Please try again later or contact support.';
+
+  @override
+  String get httpsErrorFailedPrecondition => 'The request could not be completed due to a pre-existing condition. Please check your input and try again.';
+
+  @override
+  String get httpsErrorAborted => 'The action was aborted. Please try again later.';
+
+  @override
+  String get httpsErrorOutOfRange => 'The request is outside the allowable range. Please check your input and try again.';
+
+  @override
+  String get httpsErrorUnimplemented => 'This feature is not currently supported. Please check back later.';
+
+  @override
+  String get httpsErrorInternal => 'An internal server error has occurred. Please try again later or contact support.';
+
+  @override
+  String get httpsErrorUnavailable => 'The service is currently unavailable. Please try again later.';
+
+  @override
+  String get httpsErrorDataLoss => 'Data loss has occurred. Please contact support immediately.';
+
+  @override
+  String get httpsErrorUnauthenticated => 'You are not authenticated. Please log in or provide valid credentials to continue.';
+
+  @override
+  String get unknownError => 'An unknown error occurred. Please try again or contact support for assistance.';
 }
---
+++ packages/zero_flutter/lib/src/utils/show_firebase_error.dart
@@ -0,0 +1,351 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:zero_flutter/zero_flutter.dart';
+
+/// Shows a Firebase exception using the showError() function.
+///
+showFirebaseError(
+  BuildContext context,
+  FirebaseException error,
+  Map<String, String>? overrides,
+) {
+  if (overrides?.containsKey(error.code) == true) {
+    showError(context, error: overrides![error.code]!);
+    return;
+  }
+
+  final ZeroUIAppLocalizations l10n = ZeroUIAppLocalizations.of(context)!;
+  switch (error.code) {
+    case "auth/admin-restricted-operation":
+      showError(context, error: l10n.authErrorAdminRestrictedOperation);
+      break;
+    case "auth/argument-error":
+      showError(context, error: l10n.authErrorArgumentError);
+      break;
+    case "auth/app-not-authorized":
+      showError(context, error: l10n.authErrorAppNotAuthorized);
+      break;
+    case "auth/app-not-installed":
+      showError(context, error: l10n.authErrorAppNotInstalled);
+      break;
+    case "auth/captcha-check-failed":
+      showError(context, error: l10n.authErrorCaptchaCheckFailed);
+      break;
+    case "auth/code-expired":
+      showError(context, error: l10n.authErrorCodeExpired);
+      break;
+    case "auth/cors-unsupported":
+      showError(context, error: l10n.authErrorCorsUnsupported);
+      break;
+    case "auth/credential-already-in-use":
+      showError(context, error: l10n.authErrorCredentialAlreadyInUse);
+      break;
+    case "auth/custom-token-mismatch":
+      showError(context, error: l10n.authErrorCustomTokenMismatch);
+      break;
+    case "auth/requires-recent-login":
+      showError(context, error: l10n.authErrorRequiresRecentLogin);
+      break;
+    case "auth/dependent-sdk-initialized-before-auth":
+      showError(
+        context,
+        error: l10n.authErrorDependentSdkInitializedBeforeAuth,
+      );
+      break;
+    case "auth/dynamic-link-not-activated":
+      showError(context, error: l10n.authErrorDynamicLinkNotActivated);
+      break;
+    case "auth/email-change-needs-verification":
+      showError(context, error: l10n.authErrorEmailChangeNeedsVerification);
+      break;
+    case "auth/email-already-in-use":
+      showError(context, error: l10n.authErrorEmailAlreadyInUse);
+      break;
+    case "auth/emulator-config-failed":
+      showError(context, error: l10n.authErrorEmulatorConfigFailed);
+      break;
+    case "auth/expired-action-code":
+      showError(context, error: l10n.authErrorExpiredActionCode);
+      break;
+    case "auth/cancelled-popup-request":
+      showError(context, error: l10n.authErrorCancelledPopupRequest);
+      break;
+    case "auth/internal-error":
+      showError(context, error: l10n.authErrorInternalError);
+      break;
+    case "auth/invalid-api-key":
+      showError(context, error: l10n.authErrorInvalidApiKey);
+      break;
+    case "auth/invalid-app-credential":
+      showError(context, error: l10n.authErrorInvalidAppCredential);
+      break;
+    case "auth/invalid-app-id":
+      showError(context, error: l10n.authErrorInvalidAppId);
+      break;
+    case "auth/invalid-user-token":
+      showError(context, error: l10n.authErrorInvalidUserToken);
+      break;
+    case "auth/invalid-auth-event":
+      showError(context, error: l10n.authErrorInvalidAuthEvent);
+      break;
+    case "auth/invalid-cert-hash":
+      showError(context, error: l10n.authErrorInvalidCertHash);
+      break;
+    case "auth/invalid-verification-code":
+      showError(context, error: l10n.authErrorInvalidVerificationCode);
+      break;
+    case "auth/invalid-continue-uri":
+      showError(context, error: l10n.authErrorInvalidContinueUri);
+      break;
+    case "auth/invalid-cordova-configuration":
+      showError(context, error: l10n.authErrorInvalidCordovaConfiguration);
+      break;
+    case "auth/invalid-custom-token":
+      showError(context, error: l10n.authErrorInvalidCustomToken);
+      break;
+    case "auth/invalid-dynamic-link-domain":
+      showError(context, error: l10n.authErrorInvalidDynamicLinkDomain);
+      break;
+    case "auth/invalid-email":
+      showError(context, error: l10n.authErrorInvalidEmail);
+      break;
+    case "auth/invalid-emulator-scheme":
+      showError(context, error: l10n.authErrorInvalidEmulatorScheme);
+      break;
+    case "auth/invalid-credential":
+      showError(context, error: l10n.authErrorInvalidCredential);
+      break;
+    case "auth/invalid-message-payload":
+      showError(context, error: l10n.authErrorInvalidMessagePayload);
+      break;
+    case "auth/invalid-multi-factor-session":
+      showError(context, error: l10n.authErrorInvalidMultiFactorSession);
+      break;
+
+    case "auth/invalid-oauth-client-id":
+      showError(context, error: l10n.authErrorInvalidOAuthClientId);
+      break;
+    case "auth/invalid-oauth-provider":
+      showError(context, error: l10n.authErrorInvalidOAuthProvider);
+      break;
+    case "auth/invalid-action-code":
+      showError(context, error: l10n.authErrorInvalidActionCode);
+      break;
+    case "auth/unauthorized-domain":
+      showError(context, error: l10n.authErrorUnauthorizedDomain);
+      break;
+    case "auth/wrong-password":
+      showError(context, error: l10n.authErrorWrongPassword);
+      break;
+    case "auth/invalid-persistence-type":
+      showError(context, error: l10n.authErrorInvalidPersistenceType);
+      break;
+    case "auth/invalid-phone-number":
+      showError(context, error: l10n.authErrorInvalidPhoneNumber);
+      break;
+    case "auth/invalid-provider-id":
+      showError(context, error: l10n.authErrorInvalidProviderId);
+      break;
+    case "auth/invalid-recipient-email":
+      showError(context, error: l10n.authErrorInvalidRecipientEmail);
+      break;
+    case "auth/invalid-sender":
+      showError(context, error: l10n.authErrorInvalidSender);
+      break;
+    case "auth/invalid-verification-id":
+      showError(context, error: l10n.authErrorInvalidVerificationId);
+      break;
+    case "auth/invalid-tenant-id":
+      showError(context, error: l10n.authErrorInvalidTenantId);
+      break;
+    case "auth/multi-factor-info-not-found":
+      showError(context, error: l10n.authErrorMultiFactorInfoNotFound);
+      break;
+    case "auth/multi-factor-auth-required":
+      showError(context, error: l10n.authErrorMultiFactorAuthRequired);
+      break;
+    case "auth/missing-android-pkg-name":
+      showError(context, error: l10n.authErrorMissingAndroidPkgName);
+      break;
+    case "auth/missing-app-credential":
+      showError(context, error: l10n.authErrorMissingAppCredential);
+      break;
+    case "auth/auth-domain-config-required":
+      showError(context, error: l10n.authErrorAuthDomainConfigRequired);
+      break;
+    case "auth/missing-verification-code":
+      showError(context, error: l10n.authErrorMissingVerificationCode);
+      break;
+    case "auth/missing-continue-uri":
+      showError(context, error: l10n.authErrorMissingContinueUri);
+      break;
+    case "auth/missing-multi-factor-info":
+      showError(context, error: l10n.authErrorMissingMultiFactorInfo);
+      break;
+    case "auth/missing-multi-factor-session":
+      showError(context, error: l10n.authErrorMissingMultiFactorSession);
+      break;
+    case "auth/missing-phone-number":
+      showError(context, error: l10n.authErrorMissingPhoneNumber);
+      break;
+    case "auth/missing-verification-id":
+      showError(context, error: l10n.authErrorMissingVerificationId);
+      break;
+    case "auth/app-deleted":
+      showError(context, error: l10n.authErrorAppDeleted);
+      break;
+    case "auth/account-exists-with-different-credential":
+      showError(
+        context,
+        error: l10n.authErrorAccountExistsWithDifferentCredential,
+      );
+      break;
+    case "auth/network-request-failed":
+      showError(context, error: l10n.authErrorNetworkRequestFailed);
+      break;
+    case "auth/null-user":
+      showError(context, error: l10n.authErrorNullUser);
+      break;
+    case "auth/no-auth-event":
+      showError(context, error: l10n.authErrorNoAuthEvent);
+      break;
+    case "auth/no-such-provider":
+      showError(context, error: l10n.authErrorNoSuchProvider);
+      break;
+
+    case "auth/operation-not-allowed":
+      showError(context, error: l10n.authErrorOperationNotAllowed);
+      break;
+    case "auth/operation-not-supported-in-this-environment":
+      showError(
+        context,
+        error: l10n.authErrorOperationNotSupportedInThisEnvironment,
+      );
+      break;
+    case "auth/popup-blocked":
+      showError(context, error: l10n.authErrorPopupBlocked);
+      break;
+    case "auth/popup-closed-by-user":
+      showError(context, error: l10n.authErrorPopupClosedByUser);
+      break;
+    case "auth/provider-already-linked":
+      showError(context, error: l10n.authErrorProviderAlreadyLinked);
+      break;
+    case "auth/quota-exceeded":
+      showError(context, error: l10n.authErrorQuotaExceeded);
+      break;
+    case "auth/redirect-cancelled-by-user":
+      showError(context, error: l10n.authErrorRedirectCancelledByUser);
+      break;
+    case "auth/redirect-operation-pending":
+      showError(context, error: l10n.authErrorRedirectOperationPending);
+      break;
+    case "auth/rejected-credential":
+      showError(context, error: l10n.authErrorRejectedCredential);
+      break;
+    case "auth/second-factor-already-in-use":
+      showError(context, error: l10n.authErrorSecondFactorAlreadyInUse);
+      break;
+    case "auth/maximum-second-factor-count-exceeded":
+      showError(context, error: l10n.authErrorMaximumSecondFactorCountExceeded);
+      break;
+    case "auth/tenant-id-mismatch":
+      showError(context, error: l10n.authErrorTenantIdMismatch);
+      break;
+    case "auth/timeout":
+      showError(context, error: l10n.authErrorTimeout);
+      break;
+    case "auth/user-token-expired":
+      showError(context, error: l10n.authErrorUserTokenExpired);
+      break;
+    case "auth/too-many-requests":
+      showError(context, error: l10n.authErrorTooManyRequests);
+      break;
+    case "auth/unauthorized-continue-uri":
+      showError(context, error: l10n.authErrorUnauthorizedContinueUri);
+      break;
+    case "auth/unsupported-first-factor":
+      showError(context, error: l10n.authErrorUnsupportedFirstFactor);
+      break;
+    case "auth/unsupported-persistence-type":
+      showError(context, error: l10n.authErrorUnsupportedPersistenceType);
+      break;
+    case "auth/unsupported-tenant-operation":
+      showError(context, error: l10n.authErrorUnsupportedTenantOperation);
+      break;
+    case "auth/unverified-email":
+      showError(context, error: l10n.authErrorUnverifiedEmail);
+      break;
+    case "auth/user-cancelled":
+      showError(context, error: l10n.authErrorUserCancelled);
+      break;
+    case "auth/user-not-found":
+      showError(context, error: l10n.authErrorUserNotFound);
+      break;
+    case "auth/user-disabled":
+      showError(context, error: l10n.authErrorUserDisabled);
+      break;
+    case "auth/user-mismatch":
+      showError(context, error: l10n.authErrorUserMismatch);
+      break;
+    case "auth/user-signed-out":
+      showError(context, error: l10n.authErrorUserSignedOut);
+      break;
+    case "auth/weak-password":
+      showError(context, error: l10n.authErrorWeakPassword);
+      break;
+    case "auth/web-storage-unsupported":
+      showError(context, error: l10n.authErrorWebStorageUnsupported);
+      break;
+    case "auth/already-initialized":
+      showError(context, error: l10n.authErrorAlreadyInitialized);
+      break;
+    case 'cancelled':
+      showError(context, error: l10n.httpsErrorCancelled);
+      break;
+    case 'invalid-argument':
+      showError(context, error: l10n.httpsErrorInvalidArgument);
+      break;
+    case 'deadline-exceeded':
+      showError(context, error: l10n.httpsErrorDeadlineExceeded);
+      break;
+    case 'not-found':
+      showError(context, error: l10n.httpsErrorNotFound);
+      break;
+    case 'already-exists':
+      showError(context, error: l10n.httpsErrorAlreadyExists);
+      break;
+    case 'permission-denied':
+      showError(context, error: l10n.httpsErrorPermissionDenied);
+      break;
+    case 'resource-exhausted':
+      showError(context, error: l10n.httpsErrorResourceExhausted);
+      break;
+    case 'failed-precondition':
+      showError(context, error: l10n.httpsErrorFailedPrecondition);
+      break;
+    case 'aborted':
+      showError(context, error: l10n.httpsErrorAborted);
+      break;
+    case 'out-of-range':
+      showError(context, error: l10n.httpsErrorOutOfRange);
+      break;
+    case 'unimplemented':
+      showError(context, error: l10n.httpsErrorUnimplemented);
+      break;
+    case 'internal':
+      showError(context, error: l10n.httpsErrorInternal);
+      break;
+    case 'unavailable':
+      showError(context, error: l10n.httpsErrorUnavailable);
+      break;
+    case 'data-loss':
+      showError(context, error: l10n.httpsErrorDataLoss);
+      break;
+    case 'unauthenticated':
+      showError(context, error: l10n.httpsErrorUnauthenticated);
+      break;
+    default:
+      showError(context, error: l10n.unknownError);
+      break;
+  }
+}
